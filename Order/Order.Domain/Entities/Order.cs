3172e6119c3f34ad685774a14f7cf146043a888f
DESKTOP-GB680PK\mak27 <mak27arr@gmail.com>
2025-02-20 23:16:02 +0200

    add new project

---
+++ Order/Order.Domain/Entities/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace Order.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+
+        public int? ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double Price { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
