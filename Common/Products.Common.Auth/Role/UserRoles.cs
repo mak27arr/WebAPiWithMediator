843a2f00b5e1d590aadd1122934c7640df130d4b
DESKTOP-GB680PK\mak27 <mak27arr@gmail.com>
2025-02-23 14:44:54 +0200

    added auth role project

---
+++ Common/Products.Common.Auth/Role/UserRoles.cs
@@ -0,0 +1,12 @@
+﻿namespace Products.Common.Auth.Role
+{
+    [Flags]
+    public enum UserRoles
+    {
+        None = 0,
+        User = 1 << 0,
+        Manager = 1 << 1,
+        Logistics = 1 << 2,
+        Admin = 1 << 3
+    }
+}
