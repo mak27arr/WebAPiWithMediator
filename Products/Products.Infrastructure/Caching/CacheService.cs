c240ee37d6a6373853e34ff43730fef0d689a16a
DESKTOP-GB680PK\mak27 <mak27arr@gmail.com>
2025-02-24 23:50:45 +0200

    start redis

--- Products/Products.Infrastructure/Caching/CacheService.cs
+++ Products/Products.Infrastructure/Caching/CacheService.cs
@@ -1,20 +1,22 @@
 ﻿using Microsoft.Extensions.Logging;
 using Products.Infrastructure.Interfaces.Caching;
 using StackExchange.Redis;
+using System.Buffers;
 using System.Text.Json;
 
 namespace Products.Infrastructure.Caching
 {
     internal class GenericCacheService<T> : ICacheService<T> where T : class
     {
+        private readonly ConnectionMultiplexer _connection;
         private readonly IDatabase _cache;
         private readonly ILogger _loger;
         private readonly string _redisPrefix;
 
         public GenericCacheService(ILogger loger, RedisCacheConfiguration redisCacheConfiguration)
         {
-            var connection = redisCacheConfiguration.GetConnection();
-            _cache = connection.GetDatabase();
+            _connection = redisCacheConfiguration.GetConnection();
+            _cache = _connection.GetDatabase();
             _loger = loger;
             var cacheValueType = typeof(T);
             _redisPrefix = $"{cacheValueType.Namespace}.{cacheValueType.Name}";
@@ -38,7 +40,31 @@ namespace Products.Infrastructure.Caching
 
         public async Task InvalidateCacheAsync()
         {
-            throw new NotImplementedException();
+            const int batchSize = 1000;
+            var keysBatch = ArrayPool<RedisKey>.Shared.Rent(batchSize);
+
+            foreach (var endpoint in _connection.GetEndPoints())
+            {
+                var server = _connection.GetServer(endpoint);
+                var keys = server.KeysAsync(pattern: _redisPrefix + "*");
+                var index = 0;
+
+                await foreach (var key in server.KeysAsync(pattern: _redisPrefix + "*"))
+                {
+                    keysBatch[index++] = key;
+
+                    if (index >= batchSize)
+                    {
+                        await _cache.KeyDeleteAsync(keysBatch);
+                        index = 0;
+                    }
+                }
+
+                if (index > 0)
+                    await _cache.KeyDeleteAsync(keysBatch[..index]);
+            }
+
+            ArrayPool<RedisKey>.Shared.Return(keysBatch);
         }
 
         private RedisKey GetRedisKey(string key)
