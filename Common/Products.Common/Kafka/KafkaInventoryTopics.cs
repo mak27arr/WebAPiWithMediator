0bcb920d26b57a8360e5511d31946b1a1e693f22
andrii makar <mak27arr@gmail.com>
2025-03-09 21:18:35 +0200

    added kafka topics

---
+++ Common/Products.Common/Kafka/KafkaInventoryTopics.cs
@@ -0,0 +1,11 @@
+﻿namespace Products.Common.Kafka
+{
+    public static class KafkaInventoryTopics
+    {
+        public const string ProductAddedToInventory = "product-added-to-inventory";
+        public const string ProductRemovedFromInventory = "product-removed-from-inventory";
+        public const string InventoryChecked = "inventory-checked";
+        public const string InventoryReservationSucceeded = "inventory-reservation-succeeded";
+        public const string InventoryReservationFailed = "inventory-reservation-failed";
+    }
+}
---
+++ Common/Products.Common/Kafka/KafkaTopics.cs
@@ -0,0 +1,11 @@
+﻿namespace Products.Common.Kafka
+{
+    public static class KafkaOrderTopics
+    {
+        public const string OrderCreated = "order-created";
+        public const string OrderUpdated = "order-updated";
+        public const string OrderDeleted = "order-deleted";
+        public const string OrderPaid = "order-paid";
+        public const string OrderFailed = "order-failed";
+    }
+}
