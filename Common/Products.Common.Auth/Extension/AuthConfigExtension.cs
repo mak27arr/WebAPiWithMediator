296c6bc95be525b4aded7345382e9312057897d4
andrii makar <mak27arr@gmail.com>
2025-03-16 12:47:42 +0200

    update auth package for azure

---
+++ Common/Products.Common.Auth/Extension/AuthConfigExtension.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
+
+namespace Products.Common.Auth.Extension
+{
+    public static class AuthConfigExtension
+    {
+        private static readonly string _authorityConfigKey = "AzureAdB2C";
+
+        public static IServiceCollection AddAuthConfig(this IServiceCollection services, IConfiguration configuration, IHostEnvironment env)
+        {
+            if (!IsAuthAvailable(configuration))
+                return services;
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApi(configuration.GetSection(_authorityConfigKey));
+            services.AddAuthorization();
+
+            return services;
+        }
+
+        public static IApplicationBuilder ConfigureAuthentication(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            if (IsAuthAvailable(configuration))
+            {
+                app.UseAuthentication();
+                app.UseAuthorization();
+            }
+
+            return app;
+        }
+
+        private static bool IsAuthAvailable(IConfiguration configuration)
+        {
+            return !string.IsNullOrWhiteSpace(configuration[_authorityConfigKey]);
+        }
+    }
+}
