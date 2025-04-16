7b3c6c508c2115ab0572368e72ff4e92095ffe02
andrii makar <mak27arr@gmail.com>
2025-04-16 22:33:50 +0300

    add minimal API to API package

---
+++ Common/Products.Common.API/Endpoint/AddEndpointDefinition.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Inventory.API.Endpoint
+{
+    public static class AddEndpointDefinition
+    {
+        public static IServiceCollection AddEndpointDefinitions(this IServiceCollection services, Type assemblyMarkerType)
+        {
+            var endpointDefinitionTypes = assemblyMarkerType.Assembly
+                .GetTypes()
+                .Where(t => typeof(IEndpointDefinition).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+
+            foreach (var type in endpointDefinitionTypes)
+            {
+                services.AddTransient(typeof(IEndpointDefinition), type);
+            }
+
+            return services;
+        }
+
+        public static WebApplication UseEndpointDefinitions(this WebApplication app)
+        {
+            var definitions = app.Services.GetServices<IEndpointDefinition>();
+            foreach (var definition in definitions)
+            {
+                definition.RegisterEndpoints(app);
+            }
+
+            return app;
+        }
+    }
+}
---
+++ Common/Products.Common.API/Endpoint/IEndpointDefinition.cs
@@ -0,0 +1,7 @@
+﻿namespace Inventory.API.Endpoint
+{
+    public interface IEndpointDefinition
+    {
+        void RegisterEndpoints(WebApplication app);
+    }
+}
